c0c963ea693ad1c1bcd3fe76f3263beb04c2196f
Frankie Timmons <frankietimmons@Frankies-MacBook-Pro.local>
2019-03-17 14:39:30 -0400

    Use pipenv to manage dependencies

--- README.py
+++ README.py
@@ -6,3 +6,9 @@ A demo project that uses Python and NASA data to find meteor landing sites
 This project requires Python 3 and the requests packageself.
 
 'python3 find_meteors.py'
+
+```
+pipenv install
+pipenv run "python find_meteors.py"
+
+```
